a56918b0ecd74a601debe5704fe7b4337fd73270
Alexandre Ungaratti <alexandre.ungaratti@gmail.com>
2023-10-25 19:38:04 -0300

    mais coisas

--- internal/modules/ingridient/domain/entity/ingridient.go
+++ internal/modules/ingridient/domain/entity/ingridient.go
@@ -2,9 +2,12 @@ package entity
 
 import (
 	"hamburgueria/internal/modules/ingridient/domain/valueobjects"
+
+	"github.com/google/uuid"
 )
 
 type Ingridients struct {
+	Id     uuid.UUID           `json:"id"`
 	Name   valueobjects.Name   `json:"name"`
 	Amount valueobjects.Amount `json:"amount"`
 	Type   valueobjects.Type   `json:"type"`
--- internal/modules/ingridient/domain/entity/ingridient_type.go
+++ internal/modules/ingridient/domain/entity/ingridient_type.go
@@ -1,8 +1,13 @@
 package entity
 
-import "hamburgueria/internal/modules/ingridient/domain/valueobjects"
+import (
+	"hamburgueria/internal/modules/ingridient/domain/valueobjects"
+
+	"github.com/google/uuid"
+)
 
 type IngridientType struct {
+	Id              uuid.UUID                    `json:"id"`
 	Name            valueobjects.Name            `json:"name"`
 	Optional        valueobjects.Optional        `json:"optional"`
 	Max_QTD         valueobjects.Max_QTD         `json:"max_qtd"`
