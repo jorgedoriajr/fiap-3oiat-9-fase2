19c0fdb48cdc2d9e4ffbb4be5b1482c3279b8d4d
Alexandre Ungaratti <alexandre.ungaratti@gmail.com>
2024-01-22 23:48:09 -0300

    draft

--- internal/modules/payment/infra/client/mercadopago/response/mp_qr_code_payment_response.go
+++ internal/modules/payment/infra/client/mercadopago/response/mp_qr_code_payment_response.go
@@ -1,10 +1,14 @@
 package response
 
-import "hamburgueria/internal/modules/payment/domain"
+import (
+	"hamburgueria/internal/modules/payment/domain"
+
+	"github.com/google/uuid"
+)
 
 type QrCodePaymentResponse struct {
-	QrData         string `json:"qr_data"`
-	InStoreOrderId string `json:"in_store_order_id"`
+	QrData         string    `json:"qr_data"`
+	InStoreOrderId uuid.UUID `json:"in_store_order_id"`
 }
 
 func (qcpr QrCodePaymentResponse) MpQrCodeResponseToPaymentEntity() *domain.Payment {
